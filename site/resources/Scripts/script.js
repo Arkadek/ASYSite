ffb497bc684a05ba4aa0de4012b191ec8269de43
Arkadek <animisenkov@gmail.com>
2020-11-10 04:39:08 +0700

    ASYS_0.1
    Начал работу с jQuerry

--- site/resources/Scripts/script.js
+++ site/resources/Scripts/script.js
@@ -1,3 +1,19 @@
 $('document').ready(function(){
-	$('body').append('<a href="https://google.com">Перейти</a>');
+	//$('body').append('<a href="https://google.com">Перейти</a>');
+	$('#main').on('click',function(){
+		
+}
+	$('#News').on('click',function(){
+		
+}
+	$('#Projects').on('click',function(){
+		
+}
+	$('#Forum').on('click',function(){
+		
+}
+	$('#Find').on('click',function(){
+		
+}
+	});
 })
