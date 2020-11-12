77a4ed4e8d8060580e386b680e7ae2659e4b6ade
Arkadek <animisenkov@gmail.com>
2020-11-13 03:58:47 +0700

    ver_0.1.1

--- site/resources/Scripts/script.js
+++ site/resources/Scripts/script.js
@@ -12,66 +12,37 @@ $(document).ready(function(){
       $("body,html").animate({scrollTop:0},500);
       return false
     })})
+
+/*var sections = $('section')
+    , nav = $('nav')
+    , nav_height = nav.outerHeight();
+  
+  $(window).on('scroll', function () {
+    var cur_pos = $(this).scrollTop();
     
-/*
-var ticking = false;
-var isFirefox = (/Firefox/i.test(navigator.userAgent));
-var isIe = (/MSIE/i.test(navigator.userAgent)) || (/Trident.*rv\:11\./i.test(navigator.userAgent));
-var scrollSensitivitySetting = 30; //Increase/decrease this number to change sensitivity to trackpad gestures (up = less sensitive; down = more sensitive) 
-var slideDurationSetting = 600; //Amount of time for which slide is "locked"
-var currentSlideNumber = 0;
-var totalSlideNumber = $(".background").length;
-
-
-function parallaxScroll(evt) {
-  if (isFirefox) {
-    delta = evt.detail * (-120);
-  } else if (isIe) {
-    //Set delta for IE
-    delta = -evt.deltaY;
-  } else {
-    //Set delta for all other browsers
-    delta = evt.wheelDelta;
-  }
-
-  if (ticking != true) {
-    if (delta <= -scrollSensitivitySetting) {
-      //Down scroll
-      ticking = true;
-      if (currentSlideNumber !== totalSlideNumber - 1) {
-        currentSlideNumber++;
-        nextItem();
+    sections.each(function() {
+      var top = $(this).offset().top - nav_height,
+          bottom = top + $(this).outerHeight();
+      
+      if (cur_pos >= top && cur_pos <= bottom) {
+        nav.find('a').removeClass('class');
+        div.removeClass('class');
+        
+        $(this).addClass('home');
+        nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('home');
       }
-      slideDurationTimeout(slideDurationSetting);
-    }
-    if (delta >= scrollSensitivitySetting) {
-      //Up scroll
-      ticking = true;
-      if (currentSlideNumber !== 0) {
-        currentSlideNumber--;
-      }
-      previousItem();
-      slideDurationTimeout(slideDurationSetting);
-    }
-  }
-}
-
-function slideDurationTimeout(slideDuration) {
-  setTimeout(function() {
-    ticking = false;
-  }, slideDuration);
-}
-
-var mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
-window.addEventListener(mousewheelEvent, _.throttle(parallaxScroll, 60), false);
-
-function nextItem() {
-  var $previousSlide = $(".background").eq(currentSlideNumber - 1);
-  $previousSlide.removeClass("up-scroll").addClass("down-scroll");
-}
-
-function previousItem() {
-  var $currentSlide = $(".background").eq(currentSlideNumber);
-  $currentSlide.removeClass("down-scroll").addClass("up-scroll");
-}*/
+    });
+  });
+  
+  nav.find('a').on('click', function () {
+    var $el = $(this)
+      , id = $el.attr('href');
+    
+    $('html, body').animate({
+      scrollTop: $(id).offset().top - nav_height
+    }, 500);
+    
+    return false;
+  });
+*/
 
